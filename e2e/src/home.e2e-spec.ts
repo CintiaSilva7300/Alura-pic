a76a21635bd7ce0cbb40e20e7175138a28754114
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-14 11:15:53 -0300

    first commit

---
+++ e2e/src/home.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { browser, element, by, protractor } from 'protractor';
+
+describe('Home Page', () => {
+  beforeEach(async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+  });
+
+  it('should navigate to user profile', async () => {
+    const title = await browser.getTitle();
+    expect(title).toEqual('Timeline');
+    console.log('------> Teste 0');
+  });
+
+  it('should display a list of photos', async () => {
+    const photoListSize = await element.all(by.css('.photo')).count();
+    expect(photoListSize).toBeGreaterThan(0);
+    console.log('------> Teste 1');
+  });
+
+  it('should navigate to photo detail when photo navigation is triggered', async () => {
+    await element.all(by.css('.photo')).first().sendKeys(protractor.Key.ENTER);
+    const title = await browser.getTitle();
+    expect(title).toBe('Photo detail');
+    console.log('------> Teste 2');
+  });
+
+  it('should list one item when filtering by word "farol"', async () => {
+    const searchInput = element(by.css('ap-search input[type=search]'));
+    await searchInput.sendKeys('farol');
+    const list = element.all(by.css('.photo'));
+    const photoListSize = await list.count();
+    expect(photoListSize).toBe(1);
+    console.log('------> Teste 3');
+  });
+});
